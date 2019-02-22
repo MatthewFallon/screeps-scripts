0403185ab5d099341848359ebde98e3b60953927
matthew fallon <Matthew.b.fallon@gmail.com>
2019-02-22 17:51:06 -0600

    Created the ticksToSpawn calculation and helper methods.

--- status.creep.js
+++ status.creep.js
@@ -9,6 +9,15 @@
  
  
 module.exports = {
+
+    /**
+     * This helper method returns a number corresponding to the number of ticks required for a creep to move one
+     * square on plains (minimum is 1). This method is rendered useless in other contexts and should only be used for
+     * referential speed of creeps.
+     *
+     * @param creepToCalculate The creep to perform the calculation on.
+     * @return {number} The relative speed of the creep in number of ticks per 1 square of movement.
+     */
     speed: function(creepToCalculate) {
         let weight = 0;
         let movement = 0;
@@ -40,6 +49,80 @@ module.exports = {
             }
          
         }
+    },
+
+    /**
+     * Calculates the total weight of a creep as determined by whether it is carrying anything and how many
+     * non-MOVE body parts it has.
+     *
+     * @param creepToCalculate The creep to perform the calculation on.
+     * @return {number} The current calculated weight for the creep.
+     */
+    creepWeight: function(creepToCalculate) {
+        let weight = 0;
+        for (let i = 0; i < creepToCalculate.body.length; i++) {
+            if (creepToCalculate.body[i].type === CARRY) {
+                if (_.sum(creepToCalculate.carry) > 0) {
+                    weight += 1;
+                }
+            }
+            else if (creepToCalculate.body[i].type === MOVE) {
+            }
+            else {
+                weight += 1;
+            }
+
+        }
+        return weight;
+    },
+
+    /**
+     * Calculates the total number of MOVE body parts on the creep.
+     *
+     * @param creepToCalculate The creep to perform the calculation on.
+     * @return {number} The number of MOVE pieces the creep has.
+     */
+    creepMovement: function(creepToCalculate) {
+        let movement = 0;
+        for (let i = 0; i < creepToCalculate.body.length; i++) {
+            if (creepToCalculate.body[i].type === MOVE) {
+                movement += 1;
+            }
+        }
+        return movement;
+    },
+
+    /**
+     * This method will calculate the number of ticks required for a creep to reach the nearest spawn within their
+     * current room, using pos.findPathTo(), room.getTerrain(), and speed() to properly calculate the required ticks.
+     *
+     * Only works to calculate for the path in the current room.
+     *
+     * @param creepToCalculate The creep to perform calculations on.
+     * @return {number} The number of ticks that would be required for a creep to reach spawn if it started moving now.
+     * or -1 if there is no path.
+     */
+    ticksToSpawn: function (creepToCalculate) {
+        let movement = creepMovement(creepToCalculate);
+        let weight = creepWeight(creepToCalculate);
+        let time = 0;
+        const terrain = creepToCalculate.room.getTerrain();
+        for (let each in creepToCalculate.pos.findPathTo(creepToCalculate.pos.findClosestByPath(FIND_MY_SPAWNS))){
+            if (creepToCalculate.room.getPositionAt(each.x, each.y).lookFor(LOOK_STRUCTURES)[0].structureType === STRUCTURE_ROAD) {
+                time += (weight * 1) - (movement * 2);
+            }
+            else {
+                switch (terrain.get(each.x, each.y)) {
+                    case TERRAIN_MASK_WALL:
+                        return -1;
+                    case TERRAIN_MASK_SWAMP:
+                        time += (weight * 10) - (movement * 2);
+                    case 0:
+                        time += (weight * 2) - (movement * 2);
+                }
+            }
+        }
+        return time;
     }
 
 };
