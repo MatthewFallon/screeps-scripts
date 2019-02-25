22ab0ad6dd259120481edd0b23ff61ab7b467b57
matthew fallon <Matthew.b.fallon@gmail.com>
2019-02-24 19:37:55 -0600

    debugging

--- status.spawn.js
+++ status.spawn.js
@@ -12,6 +12,7 @@ module.exports = {
      */
     spawnNext: function (spawn) {
         let roomScale = (spawn.room.controller.level * Memory.scaleTo);
+        console.log(roomScale);
         if (!Memory.war) {
             console.log("not war");
             if (roomScale < spawn.room.find(FIND_MY_CREEPS).length) {
